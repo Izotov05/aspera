a94df2162b674c00db1078790c057f04a5fac02f
Izotov05 <p.o.izotov@gmail.com>
2025-06-07 13:34:11 +0300

    Update quartz.config.ts

--- quartz.config.ts
+++ quartz.config.ts
@@ -15,8 +15,8 @@ const config: QuartzConfig = {
     analytics: {
       provider: "plausible",
     },
-    locale: "en-US",
-    baseUrl: "quartz.jzhao.xyz",
+    locale: "ru",
+    baseUrl: "izotov007.github.io/aspera",
     ignorePatterns: ["private", "templates", ".obsidian"],
     defaultDateType: "modified",
     theme: {
